aa48abf8ff90d81386708edbf2b8d7a4ade454c6
drdrongo <hayatoclarke@gmail.com>
2024-06-18 23:36:25 +0900

    stars on canvas instead

--- src/components/SwooshContainer/SwooshContainer.tsx
+++ src/components/SwooshContainer/SwooshContainer.tsx
@@ -1,8 +1,8 @@
 import { styled } from "styled-components";
 import swoosh from "/src/assets/swoosh.svg";
-import { useEffect, useRef } from "react";
+import { useLayoutEffect, useMemo, useRef } from "react";
 import { ShootingStar } from "../ShootingStar";
-import { Stars } from "../Stars";
+import { createStars } from "../../utils/stars";
 
 const StyledContainer = styled.div`
   position: absolute;
@@ -33,7 +33,15 @@ export const SwooshContainer = ({
   const containerRef = useRef<HTMLDivElement>(null);
   const canvasRef = useRef<HTMLCanvasElement>(null);
 
-  useEffect(() => {
+  const containerWidth = containerRef.current?.clientWidth ?? 1000;
+  const containerHeight = containerRef.current?.clientHeight ?? 1000;
+
+  const stars = useMemo(
+    () => createStars(containerWidth, containerHeight),
+    [containerWidth, containerHeight]
+  );
+
+  useLayoutEffect(() => {
     const canvas = canvasRef.current;
     if (!canvas) return;
 
@@ -49,21 +57,49 @@ export const SwooshContainer = ({
     // Apply the gradient to the canvas context
     ctx.fillStyle = gradient;
     ctx.fillRect(0, 0, canvas.width, canvas.height);
-  }, [gradientColors]);
+
+    stars.forEach((star) => {
+      ctx.fillStyle = `rgba(255, 255, 255, ${star.opacity})`;
+      ctx.fillRect(star.x, star.y, star.size, star.size);
+
+      const shouldFadeIn =
+        currentHour >= star.fadeInHour || currentHour <= star.fadeOutHour
+          ? 1
+          : 0;
+
+      if (shouldFadeIn && star.opacity < 1) {
+        star.opacity += 0.01; // Increase opacity gradually
+      } else if (star.opacity > 0) {
+        star.opacity -= 0.01; // Decrease opacity gradually
+      }
+    });
+  }, [gradientColors, JSON.stringify(stars)]);
+
+  useLayoutEffect(() => {
+    const container = containerRef.current;
+    if (!container) return;
+
+    const canvas = canvasRef.current;
+    if (!canvas) return;
+
+    const cs = getComputedStyle(container);
+
+    /// these will return dimensions in *pixel* regardless of what
+    /// you originally specified for image:
+    const width = parseInt(cs.getPropertyValue("width"), 10);
+    const height = parseInt(cs.getPropertyValue("height"), 10);
+
+    /// now use this as width and height for your canvas element:
+    canvas.width = width;
+    canvas.height = height;
+  }, []);
 
   return (
-    <StyledContainer>
+    <StyledContainer ref={containerRef}>
       {(currentHour > 22 || currentHour <= 4) && <ShootingStar />}
-      <Stars
-        currentHour={currentHour}
-        containerWidth={(containerRef.current?.clientWidth ?? 2000) + 1000}
-      />
       <SwooshImage src={swoosh} alt="Swoosh" />
 
-      <NightSky
-        // $background={gradientColors}
-        ref={canvasRef}
-      />
+      <NightSky ref={canvasRef} />
     </StyledContainer>
   );
 };
